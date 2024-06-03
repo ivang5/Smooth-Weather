e004a13c5a8fed4bff543aac962e9bae250c2000
ivang5 <ivanprogaborov@gmail.com>
2024-06-03 20:18:51 +0200

    Added theme toggle functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { CommonService } from './services/common.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'smooth-weather';
+
+  commonService: CommonService = inject(CommonService);
 }
